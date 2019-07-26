657d436e31d84179b6e1e36d973a180cebcb1b86
Torin Blankensmith <torin.blankensmith@gmail.com>
2019-07-26 15:35:33 -0400

    downlod PNG

--- src/three-scene.js
+++ src/three-scene.js
@@ -141,4 +141,55 @@ export const renderScene = (container, guiData) => {
         mouse.y = - (pointer.clientY - rect.top) / rect.height * 2 + 1;
 
     }
+
+    function dataURIToBlob(dataURI) {
+        const binStr = window.atob(dataURI.split(',')[1]);
+        const len = binStr.length;
+        const arr = new Uint8Array(len);
+        for (let i = 0; i < len; i++) {
+            arr[i] = binStr.charCodeAt(i);
+        }
+        return new window.Blob([arr]);
+    }
+
+    function saveDataURI(name, dataURI) {
+        const blob = dataURIToBlob(dataURI);
+
+        // force download
+        const link = document.createElement('a');
+        link.download = name;
+        link.href = window.URL.createObjectURL(blob);
+        link.onclick = () => {
+            window.setTimeout(() => {
+                window.URL.revokeObjectURL(blob);
+                link.removeAttribute('href');
+            }, 500);
+        };
+        link.click();
+    }
+
+    function defaultFileName(ext) {
+        const str = `${new Date().toLocaleDateString()} at ${new Date().toLocaleTimeString()}${ext}`;
+        return str.replace(/\//g, '-').replace(/:/g, '.');
+    }
+
+    function downloadScreenShot(width, height) {
+
+        // set camera and renderer to desired screenshot dimension
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+        renderer.setSize(width, height);
+
+        renderer.render(scene, camera, null, false);
+
+        const DataURI = renderer.domElement.toDataURL('image/png');
+
+        // save
+        saveDataURI(defaultFileName('.png'), DataURI);
+        // reset to old dimensions by invoking the on window resize function
+        onWindowResize();
+    }
+
+    return { downloadScreenShot};
+    
 }
